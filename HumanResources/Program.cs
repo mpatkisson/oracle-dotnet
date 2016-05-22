054b045d49a35c5810dd0c9fb0ced608df2d1ebe
Michael Atkisson <mpatkisson@gmail.com>
2016-05-22 05:13:57 -0400

    Adds initial solution files

---
+++ HumanResources/Program.cs
@@ -0,0 +1,30 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResources
+{
+    class Program
+    {
+        private string _connectionString = ConfigurationManager.ConnectionStrings["HumanResources"].ConnectionString;
+
+        static void Main(string[] args)
+        {
+        }
+
+        private IList<Employee> GetEmployees()
+        {
+            IList<Employee> employees = new List<Employee>();
+            using (OracleConnection connection = new OracleConnection())
+            using (OracleCommand command = new OracleCommand())
+            {
+                
+            }
+            return employees;
+        }
+    }
+}
