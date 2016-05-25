9c3be65df86cfbbdc7ce8365cd11ab4ed920360a
Michael Atkisson <mpatkisson@gmail.com>
2016-05-25 02:03:38 -0400

    Uses EF strongly typed query

---
+++ HumanResources/HumanResourcesContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResources
+{
+    public class HumanResourcesContext : DbContext
+    {
+
+        public HumanResourcesContext()
+            : base()
+        {
+            Database.SetInitializer<HumanResourcesContext>(null);
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("HR");
+            modelBuilder.Entity<Employee>().ToTable("EMPLOYEES");
+            modelBuilder.Entity<Employee>().HasKey<int>(x => x.ID);
+            modelBuilder.Entity<Employee>().Property(x => x.ID).HasColumnName("EMPLOYEE_ID");
+            modelBuilder.Entity<Employee>().Property(x => x.FirstName).HasColumnName("FIRST_NAME");
+            modelBuilder.Entity<Employee>().Property(x => x.LastName).HasColumnName("LAST_NAME");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
